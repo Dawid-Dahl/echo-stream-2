5d67e8d99550829911f1c6ef7bad37fae8f34df4
Dawid Dahl <dawiddahl@gmail.com>
2020-11-03 08:54:41 +0100

    added try catch blocks in save echo streams to server state fns

--- src/server/api/utils/util.ts
+++ src/server/api/utils/util.ts
@@ -7,50 +7,63 @@ export const generateId = (length: number) => crypto.randomBytes(length).toStrin
 
 export const saveEchoStreamInServerState = (
 	serverEchoStream: (id: string, hashtag: string, active: boolean) => ServerEchoStream
-) => async (id: string, hashtag: string): Promise<boolean> => {
-	const redisGetAsync = promisify(redisClient.get).bind(redisClient);
+) => async (id: string, hashtag: string) => {
+	try {
+		const redisGetAsync = promisify(redisClient.get).bind(redisClient);
 
-	const res = await redisGetAsync("echoStreamServerState");
+		const res = await redisGetAsync("echoStreamServerState");
 
-	if (res) {
-		const echoStreamServerState = JSON.parse(res) as ServerEchoStream[];
+		if (res) {
+			const echoStreamServerState = JSON.parse(res) as ServerEchoStream[];
 
-		redisClient.set(
-			"echoStreamServerState",
-			JSON.stringify([...echoStreamServerState, serverEchoStream(id, hashtag, true)])
-		);
+			redisClient.set(
+				"echoStreamServerState",
+				JSON.stringify([...echoStreamServerState, serverEchoStream(id, hashtag, true)])
+			);
 
-		return true;
-	} else {
+			return true;
+		} else {
+			return false;
+		}
+	} catch (e) {
+		console.log(e);
 		return false;
 	}
 };
 
 export const removeStreamFromServerState = async (echoStreamId: string) => {
-	const redisGetAsync = promisify(redisClient.get).bind(redisClient);
+	try {
+		const redisGetAsync = promisify(redisClient.get).bind(redisClient);
 
-	const res = await redisGetAsync("echoStreamServerState");
+		const res = await redisGetAsync("echoStreamServerState");
 
-	if (res) {
-		const echoStreamServerState = JSON.parse(res) as ServerEchoStream[];
+		if (res) {
+			const echoStreamServerState = JSON.parse(res) as ServerEchoStream[];
 
-		redisClient.set(
-			"echoStreamServerState",
-			JSON.stringify(echoStreamServerState.filter(stream => stream.id !== echoStreamId))
-		);
-	} else {
-		throw new Error("Couldn't get the stream server state from Redis.");
+			redisClient.set(
+				"echoStreamServerState",
+				JSON.stringify(echoStreamServerState.filter(stream => stream.id !== echoStreamId))
+			);
+		} else {
+			throw new Error("Couldn't get the stream server state from Redis.");
+		}
+	} catch (e) {
+		console.log(e);
 	}
 };
 
 export const removeAllStreamsFromServerState = async () => {
-	const redisGetAsync = promisify(redisClient.get).bind(redisClient);
+	try {
+		const redisGetAsync = promisify(redisClient.get).bind(redisClient);
 
-	const res = await redisGetAsync("echoStreamServerState");
+		const res = await redisGetAsync("echoStreamServerState");
 
-	if (res) {
-		redisClient.set("echoStreamServerState", JSON.stringify([]));
-	} else {
-		throw new Error("Couldn't get the stream server state from Redis.");
+		if (res) {
+			redisClient.set("echoStreamServerState", JSON.stringify([]));
+		} else {
+			throw new Error("Couldn't get the stream server state from Redis.");
+		}
+	} catch (e) {
+		console.log(e);
 	}
 };
