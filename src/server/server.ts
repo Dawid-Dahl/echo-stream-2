330a193d41d9369ddc2715424c7cb438a4217da2
Dawid Dahl <dawiddahl@gmail.com>
2020-11-08 15:49:58 +0100

    deploying to Heroku

--- src/server/server.ts
+++ src/server/server.ts
@@ -46,7 +46,7 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("dist/client"));
+	app.use(express.static(path.join(__dirname, "../", "client")));
 
 	app.get("/*", (req, res) => {
 		res.sendFile(path.join(__dirname, "../", "client", "index.html"));
