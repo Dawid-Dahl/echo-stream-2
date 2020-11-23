b97ad1791312eb253400b010f8c3e328e444d392
Dawid Dahl <dawiddahl@gmail.com>
2020-11-23 18:54:30 +0100

    cleaned up server file

--- src/server/server.ts
+++ src/server/server.ts
@@ -13,29 +13,27 @@ export const server = (effectContainer: EffectContainer) => {
 
 	const app = express();
 
-	if (process.env.NODE_ENV !== "test") {
-		const RedisStore = require("connect-redis")(session);
+	const RedisStore = require("connect-redis")(session);
 
-		if (!process.env.REDIS_URL) {
-			throw new Error("REDIS_URL not found.");
-		}
-
-		app.use(
-			session({
-				store: new RedisStore({client: redisClient}),
-				secret: process.env.SESSION_STORE_SECRET as string,
-				saveUninitialized: true,
-				resave: false,
-			})
-		);
-
-		app.use(function (req, res, next) {
-			if (!req.session) {
-				return next(new Error("Couldn't start a session. Is Redis server active?"));
-			}
-			next();
-		});
+	if (!process.env.REDIS_URL) {
+		throw new Error("REDIS_URL not found.");
 	}
+
+	app.use(
+		session({
+			store: new RedisStore({client: redisClient}),
+			secret: process.env.SESSION_STORE_SECRET as string,
+			saveUninitialized: true,
+			resave: false,
+		})
+	);
+
+	app.use(function (req, res, next) {
+		if (!req.session) {
+			return next(new Error("Couldn't start a session. Is Redis server active?"));
+		}
+		next();
+	});
 
 	app.use(express.json());
 	app.use(
